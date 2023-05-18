79de6b56765dfec031197bf9ebefb5d47a95547e
Zack Zellto <zacharyzellto@gmail.com>
2023-05-18 10:57:17 -0600

    removed hardcoded port for heroku

--- app.py
+++ app.py
@@ -65,5 +65,5 @@ def waitlist_item_route(id):
 
 
 if __name__ == '__main__':
-    app.run(port=int(os.environ.get('PORT', 5000)), debug=True)
+    app.run(host='0.0.0.0', port=int(os.environ.get('PORT', 5000)), debug=True)
     print("Flask app is running on port 5000")  # Logging statement
